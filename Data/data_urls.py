523d5c3f1dd18313e1d000a70ef6966b8f71fec4
fj0lverkr <nils.nahooy@live.com>
2020-08-25 08:29:57 +0200

    Initial commit

---
+++ Data/data_urls.py
@@ -0,0 +1,12 @@
+# Confirmed cases by date, age, sex and province
+CONFIRMED_DETAILED = "https://epistat.sciensano.be/Data/COVID19BE_CASES_AGESEX.json"
+# Cumulative number of confirmed cases by municipality
+CUMULATIVE_CONFIRMED = "https://epistat.sciensano.be/Data/COVID19BE_CASES_MUNI_CUM.json"
+# Confirmed cases by date and municipality
+CONFIRMED = "https://epistat.sciensano.be/Data/COVID19BE_CASES_MUNI.json"
+# Hospitalisations by date and provinces
+HOSPITALISATIONS = "https://epistat.sciensano.be/Data/COVID19BE_HOSP.json"
+# Mortality by date, age, sex, and region
+MORTALITY = "https://epistat.sciensano.be/Data/COVID19BE_MORT.json"
+# Total number of tests by date
+TOTAL_TESTS = "https://epistat.sciensano.be/Data/COVID19BE_tests.json"
---
+++ main.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+
+if __name__ == "__main__":
+    pass
