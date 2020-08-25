77fc41f2b3963f50da0c6fbff23faef95c8a6d0b
fj0lverkr <nils.nahooy@live.com>
2020-08-25 10:07:58 +0200

    Plotted first data set (Mortality)

--- main.py
+++ main.py
@@ -1,7 +1,39 @@
-import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
 
+from Data.data_urls import *
+
+
+def plot_covid_19_mortality():
+    # Load data and print columns (for reference)
+    data = pd.read_json(MORTALITY)
+    print(data.columns)
+
+    # Group data and verify by printing aggregated DEATHS
+    grouped_data = data.groupby("DATE")
+    print(grouped_data["DEATHS"].agg(np.sum))
+
+    # Create figure and add axes
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=list(grouped_data.groups), y=list(grouped_data["DEATHS"].agg(np.sum))))
+
+    # Set title
+    fig.update_layout(title_text="Covid-19 Evolution over time in Belgium")
+
+    # Add range slider
+    fig.update_layout(xaxis=dict(rangeselector=dict(buttons=list([dict(count=1, label="1m", step="month",
+                                                                       stepmode="backward"), dict(count=6, label="6m",
+                                                                                                  step="month",
+                                                                                                  stepmode="backward"),
+                                                                  dict(count=1, label="YTD", step="year",
+                                                                       stepmode="todate"),
+                                                                  dict(count=1, label="1y", step="year",
+                                                                       stepmode="backward"), dict(step="all")])),
+                                 rangeslider=dict(visible=True), type="date"))
+
+    fig.show()
 
 
 if __name__ == "__main__":
-    pass
+    plot_covid_19_mortality()
