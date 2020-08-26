37b7c7a58201597a3161e2a3a203ce9bdd8c757a
fj0lverkr <nils.nahooy@live.com>
2020-08-26 22:27:06 +0200

    made some adjustments for compatibility with pythoneverywhere.com

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import dash
 from Utils.DataPlotter import DataPlotter
 from Utils.DataFeeder import DataFeeder
 
-if __name__ == "__main__":
-    graph_title = "Covid-19 evolution over time in Belgium."
-    figure_matrix = [[1], [2, 3]]
-    graph = DataPlotter(DataFeeder.feed(), graph_title, figure_matrix)
-    external_stylesheets = ['https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css']
-    app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-    app.layout = graph.plot_graph()
-    app.run_server(debug=False)
+
+graph_title = "Covid-19 evolution over time in Belgium."
+figure_matrix = [[1], [2, 3]]
+graph = DataPlotter(DataFeeder.feed(), graph_title, figure_matrix)
+external_stylesheets = ['https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.layout = graph.plot_graph()
+server = app.server
+
+# comment this out when running this through a WSGI file:
+app.run_server(debug=False)
