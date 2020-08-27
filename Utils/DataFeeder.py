0be21b11257cbaccb0d6cf2fa36da79aa5f02b60
fj0lverkr <nils.nahooy@live.com>
2020-08-27 22:24:15 +0200

    Added data for the final graph

--- Utils/DataFeeder.py
+++ Utils/DataFeeder.py
@@ -28,6 +28,11 @@ def get_tests_by_date():
     return _data.groupby("DATE")
 
 
+def get_mortality_by_age_sex():
+    _data = pd.read_json(MORTALITY)
+    return _data.groupby(["AGEGROUP", "SEX"])
+
+
 class DataFeeder:
     @classmethod
     def feed(cls):
@@ -39,6 +44,8 @@ class DataFeeder:
         hosp_icu = {"data": get_hosp_by_date(), "key": "TOTAL_IN_ICU", "grouped_by": "DATE", "target_fig": 1}
         hosp_total = {"data": get_hosp_by_date(), "key": "TOTAL_IN", "grouped_by": "DATE", "target_fig": 1}
         tests = {"data": get_tests_by_date(), "key": "TESTS_ALL", "grouped_by": "DATE", "target_fig": 1}
+        mort_age_sex = {"data": get_mortality_by_age_sex(), "key": "DEATHS", "grouped_by": "AGE", "grouped_by_2": "SEX",
+                        "target_fig": 3}
 
         # the sequence of returning data is important as this will otherwise screw with the labels
-        return [cases_date, hosp_in, hosp_out, hosp_icu, hosp_total, mort, tests, cases_province]
+        return [cases_date, hosp_in, hosp_out, hosp_icu, hosp_total, mort, tests, cases_province, mort_age_sex]
