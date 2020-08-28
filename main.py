d751f33ccd0cb6465f7e34b59620203a732e6a3a
fj0lverkr <nils.nahooy@live.com>
2020-08-28 12:36:46 +0200

    Updated parts of the Dash app and added local stylesheet.

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from Utils.DataPlotter import DataPlotter
 from Utils.DataFeeder import DataFeeder
 
 
+dashboard_heading = "Covid-19 dashboard Belgium."
 figure_matrix = [[1], [2, 3]]
-graph_titles = ["Covid-19 evolution over time in Belgium.", "Cases per province.", "Mortality by age group and sex"]
-graph = DataPlotter(DataFeeder.feed(), graph_titles, figure_matrix)
-external_stylesheets = ['https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-app.layout = graph.plot_graph()
+graph_titles = ["Evolution over time.", "Cases per province.", "Mortality by age group and sex."]
+dashboard = DataPlotter(DataFeeder.feed(), dashboard_heading, graph_titles, figure_matrix)
+external_stylesheets = ['https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css',
+                        'https://fonts.googleapis.com/css2?family=Work+Sans:wght@200;300;400&display=swap']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets, title="Covid-19 Belgium")
+app.layout = dashboard.plot_graph()
 server = app.server
 
 # comment this out when running this through a WSGI file:
